4aae7d3b3ece54ca8ecdedf4d04aa8cab047eb78
Geekboii <99346437+Geekbooi@users.noreply.github.com>
2023-01-23 01:30:57 -0600

    Modern Programming Practices more about polymorphism and binding

--- src/Late_early_binding/Child.java
+++ src/Late_early_binding/Child.java
@@ -4,9 +4,8 @@ public class Child extends Parent{
     public static void main(String[] args) {
         Parent P = new Child();
         P.print(); // Early Binding happen for static
-        P.display(); // Late Binding happen for non-static, non-private
     }
-
+    @Override
     public static void print(){
         System.out.println("Child Static");
     }
